a2948c93973c9d5d9366a01af1c8a092d0df0436
Donal George <dgeorge7@binghamton.edu>
2024-08-26 15:08:30 -0400

    old

--- src/button.py
+++
@@ -1,9 +0,0 @@
-import pygame
-class Button(pygame.sprite.Sprite):
-    def __init__(self,  pos, image, altimg):
-          pygame.sprite.Sprite.__init__(self) 
-          self.images = [pygame.image.load(image), pygame.image.load(altimg)]
-          self.image = self.images[0] 
-          self.rect = self.image.get_rect()
-          self.rect.x = pos[0]
-          self.rect.y = pos[1]
